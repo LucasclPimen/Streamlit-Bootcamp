0ba0742f599e43fc31a6745afc68b343f2198d17
LucasclPimen <lucasleitaopimentel@gmail.com>
2024-10-06 22:52:24 -0300

    Submetendo o Projeto

---
+++ app.py
@@ -0,0 +1,137 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import webbrowser
+
+# Importando as funções
+
+from functions import *
+
+# Configurações básicas
+
+st.set_page_config(
+    page_title="Dashboard",
+    page_icon="📊",
+    layout="wide")
+
+st.markdown("<h1 style='text-align: center;'>📊Sales Report - Adidas</h1>", unsafe_allow_html=True)
+
+# Sidebar
+
+# Centralizando os botões horizontalmente
+st.markdown(
+    """
+    <style>
+    .stButton > button {
+        display: block;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <style>
+    .stDownloadButton > button {
+        display: block;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.sidebar.markdown("<h1 style='text-align: center;'>👾Bootcamp - Streamlit </h1>", unsafe_allow_html=True)
+
+st.sidebar.write('')
+
+btn = st.sidebar.button('Liga de Data Science', type = 'primary')
+
+if btn:
+    webbrowser.open_new_tab('https://linktr.ee/ligadsunicamp?utm_source=linktree_profile_share&ltsid=bcdbacaf-d0b6-48f7-9aa3-897fac981740')
+
+st.sidebar.write("")
+
+@st.cache_data # Previne que a operação seja executada sempre que o programa for re-executado
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+download = st.sidebar.download_button('Download data as CSV', data = convert_df(df), file_name='Adidas.csv', mime = 'text/csv')
+
+st.sidebar.divider()
+
+region = st.sidebar.selectbox('Region', df['Region'].unique())
+state = st.sidebar.selectbox('State', df['State'].unique())
+product = st.sidebar.selectbox('Product', df['Product'].unique())
+retailer = st.sidebar.selectbox('Retailer', df['Retailer'].unique())
+
+st.divider()
+
+
+
+# Dashboard
+with st.container():
+
+    col1, col2 = st.columns([5,5])
+    with col1.container(border=True):
+
+        st.markdown("<h3 style='text-align: center;'>Consolidated Data </h3>", unsafe_allow_html=True)
+        st.write("")
+        st.write("")
+        st.markdown("###### All products, regions and retailers", unsafe_allow_html=True)
+        subcol1, subcol2 = st.columns([1,1])
+        #col1.plotly_chart(state_sales(df),config={'displayModeBar': False}, use_container_width=True)
+
+        subcol1.metric(label = 'Profit', value=f"$ {round(df['Operating Profit'].sum()/1000000,2)} M")
+        subcol2.metric(label = 'Sales', value = f"{round(df['Total Sales'].sum()/1000000,2)} M")
+
+        #st.markdown("<br><br>", unsafe_allow_html=True)
+        
+        st.write("")
+
+        st.markdown("###### By product, region and retailer", unsafe_allow_html=True)
+        subcol3, subcol4 = st.columns([1,1])
+        subcol3.metric(label = 'Profit', value=f'$ {round(total_profit(df, retailer, product, region)/1000000,3)} M', 
+                    delta = f"{round(total_profit(df, retailer, product, region) / df['Operating Profit'].sum() * 100, 2)}%")
+        subcol4.metric(label = 'Sales', value = f'{round(total_sales(df, retailer, product, region)/1000,2)} K',
+                    delta = f"{round(total_sales(df, retailer, product, region)/df['Total Sales'].sum()*100, 4)}%")    
+        
+        st.write("")
+
+        st.markdown("""*Note: the green arrow does not represent a positive variation. It just points to the data directly above, meaning that 
+                    the the filtered data is equivalent to the percentage (next to the arrow) of the total amount.*""")
+        
+        st.write("")
+
+        
+    with col2.container(border= True):
+        st.markdown("<h3 style='text-align: center;'>Sales Over Time </h3>", unsafe_allow_html=True)
+        st.plotly_chart(sales_timeseries(df, retailer, product, region), config={'displayModeBar': False}, use_container_width=True)
+
+
+with st.container(border = True):
+    st.markdown("<h3 style='text-align: center;'>Product Sales by State </h3>", unsafe_allow_html= True)
+    st.plotly_chart(state_sales_distribution(df, product, retailer), config = {"displayModeBar":False}, use_container_width=True)
+
+col3, col4 = st.columns([1,1])
+
+with col3.container(border = True):
+    st.markdown("<h3 style='text-align: center;'>Sales Method </h3 ", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'>By State, product and retailer </h6", unsafe_allow_html=True)
+
+    st.plotly_chart(sales_method_distribution(df, product, state, retailer), config = {'displayModeBar': False}, use_container_width= True)
+
+with col4.container(border = True):
+    st.markdown("<h3 style='text-align: center;'>Total sales per state", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'> No filters </h6", unsafe_allow_html=True)
+    st.plotly_chart(state_sales(df), config={'displayModeBar': False}, use_container_width= True) 
+
+with st.container(border=True):
+    st.markdown("<h3 style='text-align: center;'>Pareto: Units Sold </h3 ", unsafe_allow_html=True)
+    st.plotly_chart(pareto(df), use_container_width=True)
---
+++ functions.py
@@ -0,0 +1,167 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+# Importando e preparando a base de dados
+
+df = pd.read_excel('Adidas.xlsx')
+df.drop(columns = ['RetailerID'], inplace = True)
+df.rename(columns = {'InvoiceDate': 'Invoice Date', 'PriceperUnit': 'Unit Price', 'UnitsSold': 'Units Sold', 'TotalSales': 'Total Sales',
+                     'OperatingProfit':'Operating Profit','OperatingMargin':'Operating Margin', 'SalesMethod': 'Sales Method'},
+                     inplace= True)
+
+# 1. Funções de dados consolidados: (indicadores)
+
+## Lucro total por Retailer, Produto e Região
+def total_profit(df, retailer, product, region):
+    df_filtered = df[df['Region'] == region]
+    df_filtered_aux = df_filtered[df_filtered['Product'] == product]
+    df_filtered_aux_final = df_filtered_aux[df_filtered_aux['Retailer'] == retailer]
+
+    return round(df_filtered_aux_final['Operating Profit'].sum(),3)
+
+
+## Vendas por Retailer, Produto e Região
+def total_sales(df, retailer, product, region):
+    df_filtered = df[df['Region'] == region]
+    df_filtered_aux = df_filtered[df_filtered['Product'] == product]
+    df_filtered_aux_final = df_filtered_aux[df_filtered_aux['Retailer'] == retailer]
+
+    return df_filtered_aux_final['Units Sold'].sum()
+
+# 2. Vendas ao longo do tempo
+
+def sales_timeseries(df, retailer, product, region):
+    df_filtered = df[df['Region'] == region]
+    df_filtered_aux = df_filtered[df_filtered['Product'] == product]
+    df_filtered_aux_final = df_filtered_aux[df_filtered_aux['Retailer'] == retailer]
+    df_filtered_aux_final = df_filtered_aux_final.groupby('Invoice Date')['Units Sold'].sum().reset_index()
+    
+    # Criando o gráfico:
+
+ 
+    fig = px.line(
+        df_filtered_aux_final,
+        x="Invoice Date",
+        y="Units Sold",
+        template = 'plotly_dark',
+        hover_data = ['Units Sold'],
+        title=f'{product} by {retailer} in {region}',
+        #labels = 
+    )
+
+
+
+    return fig
+
+# 3. Vendas de um produto e Retailer distribuídas por estados
+def state_sales_distribution(df, product, retailer):
+    df_filtered= df[df['Product'] == product]
+    df_filtered_aux = df_filtered[df_filtered['Retailer'] == retailer]
+
+    df_filtered_aux = df_filtered_aux.groupby('State')[['Units Sold', 'Total Sales']].sum().reset_index()
+
+    # Gerando o gráfico:
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x = df_filtered_aux["State"], y = df_filtered_aux["Total Sales"], name = "Total Sales"))
+    fig.add_trace(go.Scatter(x=df_filtered_aux["State"], y = df_filtered_aux["Units Sold"], mode = "lines",
+                            name ="Units Sold", yaxis="y2"))
+    fig.update_layout(
+        title = "Total Sales and Units Sold by State",
+        xaxis = dict(title="State"),
+        yaxis = dict(title="Total Sales", showgrid = False),
+        yaxis2 = dict(title="Units Sold", overlaying = "y", side = "right"),
+        template = "plotly_dark",
+        legend = dict(x=1,y=1.1)
+    )
+
+    return fig
+
+
+# 4. Pizza de sales method estratificado por produto, estado e retailer
+
+def sales_method_distribution(df, product, state, retailer):
+    df_filtered = df[df['State'] == state]
+    df_filtered_aux = df_filtered[df_filtered['Product'] == product]
+    df_filtered_aux_final = df_filtered_aux[df_filtered_aux['Retailer'] == retailer]
+
+    pie = df_filtered_aux_final['Sales Method'].value_counts()
+
+    # Gerando a Figura
+    fig = px.pie(
+        pie,
+        values='count',
+        names=list(pie.index),
+        labels= {'Sales Method': 'Sales Method'},
+        title=f'{product} by {retailer} in {state}',
+        template = 'plotly_dark'
+    )
+
+    fig.update_traces(hovertemplate='<b>%{label}</b><br>Count: %{value}<br>Percent: %{percent}')
+
+    return fig
+
+
+# 5. Gráfico de barras com as vendas totais por estado (sem filtros)
+
+def state_sales(df):
+    df_filtered = df.groupby('Region')['Total Sales'].sum().reset_index()
+
+    fig = px.bar(
+        df_filtered,
+        x="Region",
+        y="Total Sales",
+        title="State Sales",
+        template = 'plotly_dark'
+    )
+    return fig
+
+
+# 6. Gráfico de Pareto (sem filtros)
+
+def pareto(df):
+    df_pareto = pd.DataFrame(df.groupby('Product')['Units Sold'].sum())
+    df_pareto.reset_index(inplace = True)
+    df_pareto.sort_values('Units Sold', ascending= False, inplace= True)
+    df_pareto['Cumulative Sales'] = df_pareto['Units Sold'].cumsum()
+    df_pareto['Relative Cumulative Sales'] = df_pareto['Cumulative Sales']/(df_pareto['Units Sold'].sum())
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df_pareto['Product'],
+        y=df_pareto['Units Sold'],
+        name='Units Sold',
+
+    ))
+    fig.add_trace(
+        go.Scatter(
+            x=df_pareto['Product'],
+            y=df_pareto['Relative Cumulative Sales'],
+            name = 'Relative Cumulative Sales',
+            mode = 'lines',
+            line=dict(),
+            yaxis='y2' 
+        )
+
+    )
+
+    fig.update_layout(
+        title = "Pareto: Product versus unit sold",
+        xaxis = dict(title="State"),
+        yaxis = dict(title="Total Sales", showgrid = False),
+        yaxis2 = dict(title="Units Sold", overlaying = "y", side = "right"),
+        template = "plotly_dark",
+        legend = dict(x=1,y=1.1)
+    )
+
+    return fig
+
+
+
+
